92638367cb77547f20cb80e191435ca4854685a1
Thales Monteiro <tmontdev@gmail.com>
2022-05-22 11:17:16 -0300

    testing formatter

--- test/test.ts
+++ test/test.ts
@@ -1,6 +1,7 @@
 import {DateTime, timezones} from "../src/index";
+import {units} from "../src/units";
 
-let now = new DateTime().format('YYYY-MM-DD HH:mm:ss z');
+let now = new DateTime().add(130, units.day).format('YYYY-MMMM-DD HH:mm:ss.sss z');
 let algiers = new DateTime().tz(timezones.Africa_Algiers).format('YYYY-MM-DD HH:mm:ss z')
 
 console.log(now);
